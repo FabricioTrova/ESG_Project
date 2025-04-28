1092cfd25a68845f2bad387e0df20205dd223f42
Ronilson <ronilsonpereira.pro@gmail.com>
2025-04-28 17:27:57 -0300

    feature/alteração de rotas

---
+++ App/config/routes.php
@@ -0,0 +1,11 @@
+<?php
+// ESG_Project/App/config/routes.php
+
+$routes = [
+    '/' => 'dashboard',
+    '/dashboard' => 'dashboard',
+    '/charts' => 'charts',
+    '/history' => 'history'
+];
+
+return $routes;
--- App/views/history.php
+++ App/views/history.php
@@ -9,10 +9,11 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Tables</title>
+    <title>DashCarbon- History</title>
 
     <!-- Custom fonts for this template -->
-    <link href="/frontend/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link 
+        href="/frontend/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -38,7 +39,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">DashCarbon<sup>2</sup></div>
             </a>
 
             <!-- Divider -->
@@ -46,7 +47,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="/frontend/index.html">
+                <a class="nav-link" href="dashboard.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -125,14 +126,14 @@
 
             <!-- Nav Item - Charts -->
             <li class="nav-item">
-                <a class="nav-link" href="/frontend/pages/charts.html">
+                <a class="nav-link" href="dashboard.php">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Charts</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
             <li class="nav-item active">
-                <a class="nav-link" href="/frontend/pages/tables.html">
+                <a class="nav-link" href="dashboard.php">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Tables</span></a>
             </li>
