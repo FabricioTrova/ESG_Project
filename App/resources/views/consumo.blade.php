49447a6b1d58bcd4b196a401bd1f7f08e9f5b24e
Fabricio Trova <66146038+FabricioTrova@users.noreply.github.com>
2025-05-21 21:43:44 -0300

    feat/mostrandoUserLogado

--- App/resources/views/consumo.blade.php
+++ App/resources/views/consumo.blade.php
@@ -130,10 +130,12 @@
                         </li>
                         <div class="topbar-divider d-none d-sm-block"></div>
                         
-                        <!-- icone do perfil e configuraçãoo -->
+                        <!-- icone do perfil e configuraçãoo / Mostra o nome do usuário que está logado --> 
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                                {{ auth()->user()->nome }}
+                                </span>
                                 <img class="img-profile rounded-circle" src="{{ asset('img/undraw_profile.svg') }}">
                             </a>
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
