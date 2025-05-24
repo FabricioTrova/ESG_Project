4a5f74acd353dd8d995c9d59eb38b575c1bf5fbe
MarcusMartins55 <marcusmartins559@gmail.com>
2025-05-23 21:54:30 -0300

    fearture/MenuLateralTelaCadastroEmpresa

--- App/resources/views/dashboard.blade.php
+++ App/resources/views/dashboard.blade.php
@@ -71,6 +71,8 @@
                         <span>Historico de consumo</span></a>
           
                         <a class="collapse-item" href="{{url('/fonteDeConsumo')}}">Fontes de consumo</a>
+
+                        <a class="collapse-item" href="{{url('/empresas')}}">Cadastro de Empresas</a>
                     </div>
                 </div>
             </li> 
